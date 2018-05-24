4829826ae11b4d8745dfdcd647118ac497bd071e
yongxinxu <39528090+yongxinxu@users.noreply.github.com>
2018-05-24 11:07:14 -0400

    Update views.py

--- WOW/views.py
+++ WOW/views.py
@@ -2,13 +2,27 @@ from django.shortcuts import render
 
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .forms import NameForm
+from WOW.forms import UserForm
+from WOW.models import UserInfo
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
-
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            name = request.POST.get('name', None)
+            email = request.POST.get('email', None)
+            User = UserInfo.objects.update_or_create(
+                name = name,
+                email = email,
+                )
+            message = 'Thanks for your signing'
+            return render(request, 'index.html', {'message':message})
+    else:
+        form = UserForm()
+        return render(request, 'index.html', {'form':form})
+        
 def items(request):
     items = Item.objects.all()
     return render(request, 'items.html', {'items':items})
@@ -18,14 +32,3 @@ def item_view(request, title):
     return render(request, 'item.html', {'items':the_item})
     
 
-# forms
-def get_name(request):
-    if request.method == 'POST':
-        form = NameForm(request.POST)
-        if form.is_valid():
-            return HttpResponseRedirect('/thanks/')
-
-    else:
-        form = NameForm()
-
-    return render(request, 'name.html', {'form': form})
